1335ccfa277a77f62706c60fa307529429d099ac
kalpana <kalpanaudara058@gmail.com>
2026-09-14 21:41:49 +0530

    Installed typer CLI.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import typer
+
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
@@ -11,6 +13,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    typer.run(print_hi)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
